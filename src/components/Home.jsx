34d4bec9b9e87c51e7a8306dbd42b7a651b79917
rohitdhadambe <rohitdhadambe@gmail.com>
2025-03-30 23:35:59 +0530

    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-6xl font-bold text-white">
-          Hey.........! <br></br> I am  Rohit  Dhadambe!
+          Hey..........! <br></br> I am  Rohit  Dhadambe!
             <br />
             <span className="typewriter-container text-blue-500">
               <br></br>
